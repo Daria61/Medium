9d2effa99add3f46c7a8004765b2bab79093fe0d
Erdenedari <107394167+Daria61@users.noreply.github.com>
2023-01-12 12:55:04 +0800

    new

--- App.js
+++ App.js
@@ -4,81 +4,63 @@ import Modal from './modal';
 
 // {id , title, checked }
 function App(){
-  const [input, setInput] = useState('');
-  const [tasks, setTasks] = useState([])
+  const init = {
+    id:"",
+    input:"",
+    type:"",
+    isDone: false,
+    doneTotal: 0,
+  }
+  const [tasks, setTasks] = useState([init])
   const [hidden, setHidden] = useState("")
   const [modal, setModal] = useState(false)
 
-  const [doneTotal, setDoneTotal] = useState(0);
-
   const AddList = () => {
-    if(hidden.length === 0 ){
-      console.log(hidden);
-      const NewObj = {
-        id: tasks.length ,
-        title: input,
-        isDone : false
-      }
-      const NewArr = [...tasks]
-      NewArr.push(NewObj)
-      setTasks(NewArr)
-      setInput('')
-    }else{
-      console.log(hidden);
-      tasks.map((a)=>{
-        if(hidden === a.id){
-          a.title = input
-          setInput("")
-        }
-        return(
-          setHidden("")
-          
-        )
-      })
-    }
+    setTasks({...tasks, id: createID, })
   }
+
+  const createID = ()=>{
+    let abc = "ABCDEFGHIJKLMNOPQR"
+    let num = "1234567890"
+    let newStr = abc.split("")[Math.floor(Math.random()* 10 + 1)] + abc.split("")[Math.floor(Math.random()* 10 + 1)] + abc.split("")[Math.floor(Math.random()* 10 + 1)]
+    let newNum = num.split("")[Math.floor(Math.random()* 10)]+""+num.split("")[Math.floor(Math.random()* 10)]+""+num.split("")[Math.floor(Math.random()* 10)]
+    return newStr +  newNum
+  }
+
+
   const finished = (id) =>{
-    const objList = tasks.map((val) => {
+    const aa = tasks.map((val) => {
       if (val.id === id ){
-        val.isDone = !val.isDone;
+        // val.isDone = !val.isDone;
         if(val.isDone){
-          setDoneTotal( doneTotal + 1)
+         return  {...val, doneTotal: tasks.doneTotal + 1}
         } else {
-          setDoneTotal( doneTotal - 1)
+          return {...val, doneTotal: tasks.doneTotal + 1}
         }
       }
       return val 
     })
-    setTasks(objList)
+    setTasks(aa)
+
   }
 
   const remove = (id) =>{
     let survive = tasks.filter(tasks => tasks.id !== id);
     let too = 0
-    setDoneTotal(0)
-    console.log(doneTotal);
+    setTasks({...tasks, doneTotal: 0})
+
     setTasks(survive)
-    tasks.map((a)=>{
+    setTasks(tasks.map((a)=>{
       if(a.isDone === true){
         too++
       }
-      return(
-        setDoneTotal(too)
+      return({...a, doneTotal: too}
       )
-    })
+    }))
   }
 
   const edit = (id)=>{
-    tasks.map((e) =>{
-      if (e.id === id ){
-        setInput(e.title)
-        setHidden(id)
-        console.log(hidden);
-      } 
-      return (
-        tasks
-      )
-    })
+   
   }
   const modalChange=()=>{
     setModal(!modal)
@@ -89,9 +71,9 @@ function App(){
         <div className=' col-6'>
           <h1>To Do list</h1>
           Total {tasks.length}
-          <p>Done {doneTotal}</p>
+          <p>Done {tasks.doneTotal}</p>
           <div className='d-flex gap-2'>
-            <input type="text" placeholder='Add to do list' value={input} onChange={(e)=>{setInput(e.target.value)}}  className="form-control"/>
+            <input type="text" placeholder='Add to do list' value={tasks.input} onChange={(e)=>{setTasks({...tasks , input : e.target})}}  className="form-control"/>
             <input type="hidden" value={hidden}/>
             <button className='btn btn-primary' onClick={AddList}>Add</button>
             <button className='btn btn-primary' onClick={modalChange}>Modal</button>
@@ -103,7 +85,7 @@ function App(){
           return(
             <div className='row d-flex gap-2 border rounded align-items-center p-2 m-1'> 
               <input type="checkbox" checked={a.isDone} className="col-1" onChange={() => finished(a.id)}/>
-              <p className='col-8 m-0'>{a.title}</p>
+              <p className='col-8 m-0'>{a.input}</p>
               <button className='btn col-1  btn-danger' onClick={()=>remove(a.id)}>Delete</button>
               <button className='btn col-1  btn-warning' onClick={()=>edit(a.id)}>Edit</button>
             </div>
@@ -111,71 +93,10 @@ function App(){
         })}
       </div>
       <div>
-        {modal && ( <Modal modal ={modal} setModal = {modalChange} input={input} setInput={setInput}  addList={AddList} />)}
+        {modal && ( <Modal modal ={modal} setModal = {modalChange} setTasks={setTasks} tasks={tasks} addList={AddList} />)}
       </div>
     </div>
   )
   
 }
 export default App
-// import './App.css';
-// import {useState} from 'react'
-
-
-// function App(){
-//   const [list , setList] = useState('');
-//   const [tasks, setTasks] = useState([]);
-
-//   const push = () => {
-//     const newObj = {
-//       id: tasks.length,
-//       title : list,
-//       isDone: false,
-//     }
-//     const NewArr = [...tasks]
-//     NewArr.push(newObj)
-//     setTasks(NewArr)
-//     setList("")
-//   }
-//   const onDoneTask = (id) =>{
-//     const objList = tasks.map((val)=>{
-//       if( val.id === id){
-//         val.isDone = true
-//       }
-//       return val
-//     })
-//     setTasks(objList)
-//   }
-
-//   return (
-//    <div className='container'>
-//     <div className='row'>
-//       <div className='col-md-4 mt-4'>
-//         <h1>To Do List </h1>
-//        <div className=' d-flex gap-2'>
-//           <input type='text' placeholder='Add list' onChange={(e) => setList(e.target.value)} value={list} className="form-control "/>
-//           <button className='btn btn-primary' onClick={push} >Add</button> 
-//        </div>
-//       </div>
-//     </div>
-//     <div className='row mt-3'>
-//       <div >
-//          {tasks.map((e)=>(
-//           <div className='d-flex justify-content-between align-items-center'>
-//             <div className='d-flex'>
-//               <input type="checkbox" checked={e.isDone} onChange={()=> onDoneTask(e.id)}/>
-//               <h4>{e.title}</h4>
-//             </div>
-//             <div>
-//               <button className='btn btn-warning'>Edit</button>
-//               <button className='btn btn-danger'>Delete</button>
-//             </div>
-//           </div>
-//          ))}
-//       </div>
-//     </div>
-//    </div>
-//   );
-// }
-
-// export default App;
