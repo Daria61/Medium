f38bc013c7af9f835d891581f6cfd86500b168b0
Erdenedari <107394167+Daria61@users.noreply.github.com>
2023-01-13 09:32:44 +0800

    mm

--- modal.js
+++ modal.js
@@ -1,17 +1,29 @@
-const Modal =({modal, setModal, input, setInput, addList})=>{
+const Modal =({modal, setModal, addList , Obj, setObj, init, editStatus})=>{
     const dis = modal ? "block" : "none"
+    if(editStatus){
+        setObj(init)
+    }
     return (
-        <div className="modal" style={{display: dis}} onClick={setModal}>
+        <div className="modal " style={{display: dis}} onClick={setModal}>
             <div className="modal-body" onClick={(e)=> e.stopPropagation()}>
                 <div>
                     <h2>Edit</h2>
                 </div>
                 <div className="w-100">
-                    <input id="hh" className="form-control" type="text" value={input} onChange={(e)=> setInput(e.target.value)} placeholder="edit or add"/>
-                    <input type="hidden" value={""}/>
-                    <button className="btn btn-primary" onClick={addList}>Add</button>
+                    <input  className="form-control" type="text" value={Obj.input} onChange={(e)=> setObj({...Obj, input: e.target.value})} placeholder="Title here"/>
+                    <select className="form-select my-2" value={Obj.type}  onChange={(e)=> setObj({...Obj, type: e.target.value })}>
+                        <option value="0">Select</option>
+                        <option value="1">personal</option>
+                        <option value="2">work</option>
+                        <option value="3">other</option>
+                    </select>
+                    <input type="checkbox" id="imp"  className="me-2"  onChange={(e)=> setObj({...Obj, isCope: e.target.checked})}/>
+                    <label for="imp">Importend</label>
                 </div>
-                <div className="btn btn-warning" onClick={setModal}>Close</div>
+                <div className="row justify-content-between px-3">
+                <div className="btn btn-warning col-5" onClick={setModal}>Close</div>
+                <button className="btn btn-primary col-5" onClick={addList}>{editStatus? "Add": "save change"}</button>
+                </div>
             </div>
         </div>
     )
